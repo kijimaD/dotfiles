dba126524562902911b98a58fa6afc4145bb0cb3
kijima <norimaking777@gmail.com>
2021-08-08 13:03:27 +0900

    Add a manifest file

---
+++ .config/guix/manifests/desktop.scm
@@ -0,0 +1,28 @@
+;; desktop settings
+
+(specifications->manifest
+ '(;; code
+   "git"
+   "gcc-toolchain"
+   "make"
+   "pkg-config"
+   "texinfo"
+   "llvm"
+   "lld"
+   "clang"
+   "file"
+   "elfutils"
+   "go"
+
+   ;; network
+   "openssh"
+
+   ;; desktop
+   "syncthing"
+   "polybar"
+   "qutebrowser"
+
+   ;; japanese input
+   "ibus"
+   "ibus-anthy"
+   "font-adobe-source-han-sans"))
