afb245df15d755d9b226da5fc65808459199db70
kijima <norimaking777@gmail.com>
2022-01-16 17:22:05 +0900

    chore: rename

--- .config/polybar/pomodoro-bar.sh
+++ .config/polybar/pomodoro-bar.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 underline_color="%{u#99c2ff}%{+u}"
-pomo_message=$(emacsclient -e '(ruborcalor/org-pomodoro-time)' | cut -d '"' -f 2)
+pomo_message=$(emacsclient -e '(kd/org-pomodoro-time)' | cut -d '"' -f 2)
 echo ${underline_color}${pomo_message}
