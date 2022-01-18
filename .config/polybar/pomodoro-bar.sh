20ebc7b8428619420791067fa8c2978e9a4ffcd1
kijima <norimaking777@gmail.com>
2022-01-18 22:07:56 +0900

    feat: polybar settings

--- .config/polybar/pomodoro-bar.sh
+++ .config/polybar/pomodoro-bar.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-underline_color="%{u#99c2ff}%{+u}"
 pomo_message=$(emacsclient -e '(kd/org-pomodoro-time)' | cut -d '"' -f 2)
 pomo_pts=$(emacsclient -e '(kd/pmd-today-point-display)' | cut -d '"' -f 2)
-echo ${underline_color}${pomo_message}${pomo_pts}
+echo ${pomo_message}${pomo_pts}
